e6f77f9f18ae7d175b61465749406f6e3407d5c1
Aruwajoye <aaruwajo@ccibio104-207.uncc.edu>
2016-04-05 14:20:33 -0400

    i have added it

---
+++ gc_calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def gc(sequence):
+    gc_count = 0
+    for nuc in sequence:
+        if nuc == 'G':
+            gc_count += 1
+        if nuc == 'C':
+            gc_count += 1
+    return (float(gc_count) / len(sequence))
